d58448e90a112399985b9bb6bf6e0bcc35af4390
Cheng Gong <chenggong@Chengs-Laptop.local>
2024-11-01 13:29:10 -0700

    added App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import LoginPage from './components/LoginPage';
+"import React from 'react';"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Donors from './pages/Donor';
+import AddDonor from './pages/AddDonor';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <LoginPage />
-    </div>
+    <Router>
+      <div>
+        <h1>BC Cancer Donor System</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/donors" element={<Donors />} />
+          <Route path="/add-donor" element={<AddDonor />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
